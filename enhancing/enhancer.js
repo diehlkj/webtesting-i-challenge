5152e8b911f949f1e22b368d92ba30d82137a728
Ken Diehl <diehlkj.99@gmail.com>
2020-04-30 00:08:09 -0400

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,48 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    const enhanced = item.enhancement + 1;
+    return { ...item, enhancement: enhanced };
+  } else {
+    return { ...item };
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  // If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+  // If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+  // If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
+
+  let dmg5 = item.durability - 5;
+  let dmg10 = item.durability - 10;
+  let diminished = item.enhancement - 1;
+  
+  if (dmg5 < 0 || dmg10 < 0) {
+    dmg5 = 0;
+    dmg10 = 0;
+  }
+  
+  if (item.enhancement < 15) {
+    return { ...item, durability: dmg5 };
+  } 
+  else if (item.enhancement > 16) {
+    return { ...item, durability: dmg10, enhancement: diminished };
+  } 
+  else {
+    return { ...item, durability: dmg10 };
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  if (item.durability < 100) {
+    return { ...item, durability: 100 };
+  } else {
+    return { ...item };
+  }
 }
 
+// ----------------- stretch -----------------
 function get(item) {
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,135 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('enhancer functions', () => {
+    describe('function repair()', () => {
+
+        const damaged = {
+            name: "pickaxe",
+            durability: 5,
+            enhancement: 0
+        };
+
+        const repaired = {
+            name: "pickaxe",
+            durability: 100,
+            enhancement: 0
+        };
+
+        it('Should return an item with durability of 100. No other changes', () => {
+            expect(enhancer.repair(damaged)).toStrictEqual(repaired);
+        });
+    })
+
+    describe('function succeed()', () => {
+
+        it('Should return the item with its enhancement value increased by 1. No other changes.', () => {
+            
+            const base = {
+                name: "pickaxe",
+                durability: 100,
+                enhancement: 19
+            };
+
+            const enhanced = {
+                name: "pickaxe",
+                durability: 100,
+                enhancement: 20
+            };
+
+            expect(enhancer.succeed(base)).toStrictEqual(enhanced);
+        });
+
+        it('Should return the item with no change to its enhancement value. No other changes.', () => {
+            
+            const base = {
+                name: "pickaxe",
+                durability: 100,
+                enhancement: 20
+            };
+
+            const enhanced = {
+                name: "pickaxe",
+                durability: 100,
+                enhancement: 20
+            };
+
+            expect(enhancer.succeed(base)).toStrictEqual(enhanced);
+        });
+    })
+    
+    describe('function fail()', () => {
+
+        it('should decrease the durability of the item by 5', () => {
+            const base = {
+                name: "pickaxe",
+                durability: 100,
+                enhancement: 14
+            };
+    
+            const failed = {
+                name: "pickaxe",
+                durability: 95,
+                enhancement: 14
+            };
+            
+            expect(enhancer.fail(base)).toStrictEqual(failed);
+        });
+
+        it('should decrease the durability of the item by 10', () => {
+            const base = {
+                name: "pickaxe",
+                durability: 100,
+                enhancement: 15
+            };
+    
+            const failed = {
+                name: "pickaxe",
+                durability: 90,
+                enhancement: 15
+            };
+            
+            expect(enhancer.fail(base)).toStrictEqual(failed);
+        });
+
+        it('should decrease the durability of the item by 10, and the enhancement by 1', () => {
+            const base = {
+                name: "pickaxe",
+                durability: 100,
+                enhancement: 17
+            };
+    
+            const failed = {
+                name: "pickaxe",
+                durability: 90,
+                enhancement: 16
+            };
+            
+            expect(enhancer.fail(base)).toStrictEqual(failed);
+        });
+
+        it('should decrease the durability of the item to 0', () => {
+            const base = {
+                name: "pickaxe",
+                durability: 3,
+                enhancement: 17
+            };
+    
+            const failed = {
+                name: "pickaxe",
+                durability: 0,
+                enhancement: 16
+            };
+            
+            expect(enhancer.fail(base)).toStrictEqual(failed);
+        });
+    })
+    
+    // ----------------- stretch -----------------
+    // describe('function get()', () => {
+
+    //     it('should', () => {
+            
+    //     });
+    // })
+})
